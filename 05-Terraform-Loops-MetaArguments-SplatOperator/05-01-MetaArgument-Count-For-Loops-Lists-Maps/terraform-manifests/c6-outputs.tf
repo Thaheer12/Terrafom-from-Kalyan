add46c5a144c804b6fe649835e9710459bce2823
Thaheer <thaheer.t9@gmail.com>
2022-04-25 10:14:50 +0000

    update stage 5

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c6-outputs.tf
@@ -1,13 +1,29 @@
-# Terraform Output Values
-
-# EC2 Instance Public IP
-output "instance_publicip" {
-  description = "EC2 Instance Public IP"
-  value = aws_instance.myec2vm.public_ip
+# output - For Loop with List
+output "for_output_list"{
+  description = "For Loop with List"
+  value = [for instance in aws_instance.myec2vm: instance.public_dns]
 }
 
-# EC2 Instance Public DNS
-output "instance_publicdns" {
-  description = "EC2 Instance Public DNS"
-  value = aws_instance.myec2vm.public_dns
+# output - For Loop with Map
+output "for_output_map1"{
+  description = "For Loop with map"
+  value = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
+}
+
+# output - For Loop with Map2
+output "for_output_map2"{
+  description = "For Loop with map"
+  value = {for c, instance in aws_instance.myec2vm: c => instance.public_dns}
+}
+
+# output legacy splat operator(latest) - Returns the List
+output "legacy_splat_instance_publicdns"{
+  description = "Legacy splat expression"
+  value = aws_instance.myec2vm.*.public_dns
+}
+
+# output latest generalized splat operator-returns List
+output "latest_splat_instance_publicdns"{
+  description = "Generalized splat expression"
+  value = aws_instance.myec2vm [*].public_dns
 }
